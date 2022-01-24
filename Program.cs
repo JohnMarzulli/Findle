8beee65c6950a1eb3d3c2c55beb4f589847f8a03
John Marzulli <john.marzulli@hotmail.com>
2022-01-24 03:59:07 -0800

    Create simple tool to help solve Wordle.

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Findle
+{
+    public class CommandLineOptions
+    {
+        [Value(index: 0, Required = true, HelpText = "Word file")]
+        public string File { get; set; }
+
+        [Option(shortName: 'e', Required = false, HelpText = "Letters elimintated from consideration.")]
+        public string Eliminated { get; set; } = string.Empty;
+
+        [Option(shortName: 'a', Required = false, HelpText = "Letters that are anchors for the word. Use '.' as placeholders.")]
+        public string Anchors { get; set; } = string.Empty;
+
+        [Option(shortName: 'r', Required = false, HelpText = "Letters that are required for the word.")]
+        public string Required { get; set; } = string.Empty;
+    }
+
+    class Program
+    {
+        private static string[] StarterWords = {
+            "ARISE",
+            "ROAST",
+            "TEARS",
+            "MEATS",
+            "OUIJA",
+            "PIZZA",
+            "AUDIO",
+            "FARTS"
+        };
+
+        static void Main(
+            string[] args
+        )
+        {
+            bool isSuccess = Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .MapResult((CommandLineOptions opts) =>
+                {
+                    try
+                    {
+                        // We have the parsed arguments, so let's just pass them down
+                        IEnumerable<string> potentialWords = FindWords(opts).OrderBy(w => w);
+
+                        foreach (string option in potentialWords)
+                        {
+                            Console.WriteLine(option.ToUpperInvariant());
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error parsing the command line! EX={ex}");
+                    }
+
+                    return true;
+                },
+                errs => false); // Invalid arguments
+
+            if (!isSuccess)
+            {
+                Console.WriteLine("Sorry, you are error.");
+            }
+        }
+
+        private static IEnumerable<string> GetWords(
+            string file
+        )
+        {
+            IEnumerable<string> words = File.ReadLines(file);
+
+            IEnumerable<string> validWords = words
+                .Select(w => w?.Trim() ?? string.Empty)
+                .Select(w => w.ToLowerInvariant())
+                .Where(w => w.Length == 5)
+                .Where(w => !w.Any(c => !char.IsLetter(c)))
+                .Distinct();
+
+            return validWords;
+        }
+
+        private static IEnumerable<string> FindWords(
+            in CommandLineOptions options
+        )
+        {
+            string anchors = options.Anchors?.Trim().ToLowerInvariant().Replace("\"", "") ?? string.Empty;
+            char[] requiredLetters = (options.Required?.Trim().ToLowerInvariant().Replace("\"", "") ?? string.Empty).ToCharArray();
+            char[] eliminatedLetters = (options.Eliminated?.Trim().ToLowerInvariant().Replace("\"", "") ?? string.Empty).ToCharArray();
+
+            if ((options == null)
+                || (string.IsNullOrEmpty(anchors) && requiredLetters.Length == 0 && eliminatedLetters.Length == 0))
+            {
+                return StarterWords;
+            }
+
+            IEnumerable<string> words = GetWords(options.File);
+
+            // Step 1
+            //
+            // Filter for words that have required letters
+            // in required positions
+            if (anchors.Length == 5)
+            {
+                Regex match = new(anchors, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                words = words.Where(w => match.IsMatch(w));
+            }
+
+            // Step 2
+            //
+            // Filter for words that have required letters.
+            foreach (char required in requiredLetters)
+            {
+                words = words.Where(w => w.Contains(required));
+            }
+
+            // Step 3
+            //
+            // Filter out words that have letters that are fully eliminated
+            foreach (char eliminated in eliminatedLetters)
+            {
+                words = words.Where(w => !w.Contains(eliminated));
+            }
+
+            return words;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # Findle
-Tool to help solve that pesky daily word puzzle
+
+A tool to help solve that pesky daily word puzzle
+
+Written due to too much coffee, too late in the day, with too much free time... and too little sleep.
+
+## Using This
+
+To run, you will need the .Net 5 SDK.
+
+To build:
+
+`dotnet build Findle.csproj`
+
+## Examples
+
+### Get A List Of Starter Words
+
+```bash
+./Findle ./words.txt
+```
+
+### Get A List Of Words With Letters In Specific Locations
+
+Returns a list of words where 'b' is the first letter, 'a' is the third letter, and 's' is the fourth letter.
+
+The dots in the 2nd and 5th position mean that those letters could be anything.
+
+```bash
+./Findle ./words.txt -a "b.as."
+```
+
+### Get A List Of Words That DO NOT Have Eliminated Letters
+
+Words with 'o', 'u', and 'i' are removed.
+
+```bash
+./Findle ./words.txt -e "oui"
+```
+
+### Get A List Of Words That MUST Have Specific Letters
+
+Words that **DO NOT** have 'q' and 'y' are removed.
+
+```bash
+./Findle ./words.txt -r "qy"
+```
+
+### Get A List Of Words That Do Not Have Eliminated Letters, Do Contain Required Letters, And Have Letters In Specific Locations
+
+Returns a list of words that
+
+- MUST contain 'l' and 'o' (any position)
+- DO NOT contain 'h', 'a', 'p', 'f', 'i', 'e', or 'r'
+- MUST contain the letter 'l' in the 4th position.
+
+```bash
+./Findle ./words.txt -r "lo" -e "hapyfier" -a "...l."
+```
